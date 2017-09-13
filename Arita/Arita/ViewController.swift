2ec72ad5d3fb4fb279d38538b8b85dddcfd3f2bb
lihongbo <lihongbo@iot.chinamobile.com>
2017-09-13 10:40:50 +0800

    add: 创建项目

---
+++ Arita/Arita/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Arita
+//
+//  Created by 李宏博 on 2017/9/13.
+//  Copyright © 2017年 arita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
