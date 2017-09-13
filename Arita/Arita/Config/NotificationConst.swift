34a8f90962d5e944f09622428e37a6c67476708b
潘东 <pandong@iot.chinamobile.com>
2017-09-13 16:25:50 +0800

    添加相应模块文件

---
+++ Arita/Arita/Config/NotificationConst.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationConst.swift
+//  Arita
+//
+//  Created by 潘东 on 2017/9/13.
+//  Copyright © 2017年 arita. All rights reserved.
+//
+
+import Foundation
---
+++ Arita/Arita/Module/APIManager/Login&Register/LoginAPIManager.swift
@@ -0,0 +1,13 @@
+//
+//  LoginAPIManager.swift
+//  Arita
+//
+//  Created by 潘东 on 2017/9/13.
+//  Copyright © 2017年 arita. All rights reserved.
+//
+
+import UIKit
+
+class LoginAPIManager: ONAPIBaseManager {
+
+}
